1484823c312efee11d82f41372dca5efbb5c14c8
unknown <dsouzaclayton95@gmail.com>
2020-09-10 11:02:05 +0530

    cloning all my proj

---
+++ reAngular/src/main.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'app-start',
+  template: '<h1>Angular Wedcome you</h1>',
+})
+class FirstComponent {}
+
+@NgModule({
+  declarations: [FirstComponent],
+  imports: [BrowserModule],
+  bootstrap: [FirstComponent],
+})
+class FirstModule {}
+
+platformBrowserDynamic().bootstrapModule(FirstModule);
